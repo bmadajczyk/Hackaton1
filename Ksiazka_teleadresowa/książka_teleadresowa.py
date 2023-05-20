7e9ccdd664321dc031c5e8220e7106213d20b518
Bartek <madajczykb@gmail.com>
2023-05-20 14:28:13 +0200

    ksiazka teleadresowa

---
+++ Ksiazka_teleadresowa/książka_teleadresowa.py
@@ -0,0 +1,4 @@
+user_data = {'Imię i Nazwisko': {'numer telefonu': {'adres email'}}}
+user_data = input('Podaj imię, nazwisko, numer telefonu oraz adres email: ')
+user_data += user_data
+print(user_data)
